49ea8948d0e742b185cf09b898f89d2e8299ef0b
hw <hussainweb@gmail.com>
2020-07-26 16:51:50 -0400

    Add solution for raindrops

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut ret: String = "".to_owned();
+
+    if n % 3 == 0 {
+        ret.push_str("Pling")
+    }
+    if n % 5 == 0 {
+        ret.push_str("Plang")
+    }
+    if n % 7 == 0 {
+        ret.push_str("Plong")
+    }
+
+    if ret == "" {
+        ret = n.to_string();
+    }
+
+    ret.to_string()
 }
