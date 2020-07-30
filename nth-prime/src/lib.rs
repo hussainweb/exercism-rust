ca67e1d02da2d9a5345b9cb7f330729ef1a832d9
hw <hussainweb@gmail.com>
2020-07-30 11:46:48 -0400

    Add solution for nth-prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,3 +1,27 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut found_primes = 0;
+    let mut i = 2;
+    loop {
+        if is_prime(i) {
+            if found_primes == n {
+                return i;
+            }
+            found_primes += 1;
+        }
+        i += 1;
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    if n > 7 && (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0) {
+        return false;
+    }
+
+    let f: f64 = n as f64;
+    for i in 2..=f.sqrt() as u32 {
+        if n % i == 0 {
+            return false;
+        }
+    }
+    true
 }
