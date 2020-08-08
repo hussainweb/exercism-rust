257e5b777dea6839b7d208791854c8da9a68c40b
hw <hussainweb@gmail.com>
2020-08-08 14:31:37 -0400

    Add solution for space-age

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -1,21 +1,18 @@
-// The code below is a stub. Just enough to satisfy the compiler.
-// In order to pass the tests you can add-to or change any of this code.
-
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Self { seconds: s }
     }
 }
 
 pub trait Planet {
+    fn seconds_in_year() -> f64;
     fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+        d.seconds as f64 / Self::seconds_in_year()
     }
 }
 
@@ -28,11 +25,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn seconds_in_year() -> f64 {
+        31557600.0 * 0.2408467
+    }
+}
+impl Planet for Venus {
+    fn seconds_in_year() -> f64 {
+        31557600.0 * 0.61519726
+    }
+}
+impl Planet for Earth {
+    fn seconds_in_year() -> f64 {
+        31557600 as f64
+    }
+}
+impl Planet for Mars {
+    fn seconds_in_year() -> f64 {
+        31557600.0 * 1.8808158
+    }
+}
+impl Planet for Jupiter {
+    fn seconds_in_year() -> f64 {
+        31557600.0 * 11.862615
+    }
+}
+impl Planet for Saturn {
+    fn seconds_in_year() -> f64 {
+        31557600.0 * 29.447498
+    }
+}
+impl Planet for Uranus {
+    fn seconds_in_year() -> f64 {
+        31557600.0 * 84.016846
+    }
+}
+impl Planet for Neptune {
+    fn seconds_in_year() -> f64 {
+        31557600.0 * 164.79132
+    }
+}
